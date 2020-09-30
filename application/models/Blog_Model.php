a32048431958d7aa2fd90acf7d095858f8e309b0
fahtur1 <fahturrahman18ti@mahasiswa.pcr.ac.id>
2020-10-01 01:13:48 +0700

    fix error db[A

--- application/models/Blog_Model.php
+++ application/models/Blog_Model.php
@@ -6,7 +6,7 @@ class Blog_Model extends CI_Model
     public function getBlogList()
     {
         $this->db->select('*');
-        $this->db->from('u1101401_wp684.wp_post');
+        $this->db->from('u1101401_wp684.wp_posts');
         $this->db->where('post_status', 'publish');
         return $this->db->get()->result_array();
     }
