29b8149de8e21349ff12bafac051e56123c8cf94
Rahmita Ramadhanty <rahmita25@gmail.com>
2020-10-04 00:35:05 +0700

    css

--- application/views/welcome/galeri.php
+++ application/views/welcome/galeri.php
@@ -2,7 +2,7 @@
     <?php foreach ($photos as $photo) : ?>
         <div class="col-lg-4 col-md-4 col-sm-6">
             <div class="mothernail">
-                <img src="<?= base_url('assets/img/galeri/') . $photo['foto_kegiatan'] ?>" class="img-fluid" alt="Responsive image">
+                <img src="<?= base_url('assets/img/galeri/') . $photo['foto_kegiatan'] ?>" class="img-fluid galeri" alt="Responsive image">
                 <div class="mothernail-overlay">
                     <div class="text-overlay">
                         <?= $photo['ket'] ?>
